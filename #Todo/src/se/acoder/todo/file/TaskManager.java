e3730504fedac0a30d44ed77e8390f92d91c5ceb
Johannes Vestlund <iceman@westlundarna.se>
2013-03-04 13:06:34 +0100

    Improved TaskManager and also javadoc comments.
    Signed-off-by: Johannes Vestlund <iceman@westlundarna.se>

--- #Todo/src/se/acoder/todo/file/TaskManager.java
+++ #Todo/src/se/acoder/todo/file/TaskManager.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 import android.content.Context;
 
+/**
+ * The class is basically a layer between the file-storage and the application
+ * activities. It keeps track of all tasks.
+ * @author Johannes Westlund
+ *
+ */
 public class TaskManager {
 	//private final static String TAG = TaskManager.class.getSimpleName();
 	private final static String taskFileName = "taskList";
@@ -13,6 +19,7 @@ public class TaskManager {
 	
 	/**
 	 * TaskManager can only exist in one instance and is accessed through this method.
+	 * @param context Application context.
 	 * @return reference to singleton instance of TaskManager.
 	 */
 	public static TaskManager getInstance(Context context) {
@@ -26,11 +33,22 @@ public class TaskManager {
 		return instance;
 	}
 	
+	/**
+	 * Default constructor.
+	 * @param context Application context.
+	 */
 	private TaskManager(Context context){
 		fp = new FilePath(taskFileName, context);
 		fio = FileIO.getInstance();
+		if(!fio.fileExists(fp))
+			fio.create(fp);
 	}
 	
+	/**
+	 * Creates an array of strings on the format suitable
+	 * for UI adaptors.
+	 * @return The array of currently stored tasks.
+	 */
 	public String[] getTaskList(){
 		ArrayList<Task> tasks = fio.load(fp);
 		String[] taskList = new String[tasks.size()];
@@ -39,4 +57,33 @@ public class TaskManager {
 		return taskList;
 	}
 	
+	/**
+	 * Creates a new task and stores it.
+	 * @param description The description of the task.
+	 * @return A new task with the given description and a unique ID.
+	 */
+	public Task addTask(String description){
+		int lastIndex = fio.load(fp).size()-1;
+		int id;
+		if(lastIndex < 0){
+			id = 0;
+		}else{
+			id = fio.load(fp).get(lastIndex).getId();
+		}
+		Task task = new Task(id, description);
+		ArrayList<Task> tasks = new ArrayList<Task>();
+		tasks.add(task);
+		if(fio.append(fp, tasks))
+			return task;
+		return null;
+	}
+	
+	/**
+	 * Removes given task.
+	 * @param task The task to remove.
+	 * @return True if removal was successful.
+	 */
+	public boolean removeTask(Task task){
+		return fio.removeTask(fp, task);
+	}
 }
