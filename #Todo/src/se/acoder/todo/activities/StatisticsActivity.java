f6aa8902dd3b14ccc9042da6169933238d14e437
Johannes Vestlund <iceman@westlundarna.se>
2013-03-07 01:44:42 +0100

    Work mostly on statisticsactivity

--- #Todo/src/se/acoder/todo/activities/StatisticsActivity.java
+++ #Todo/src/se/acoder/todo/activities/StatisticsActivity.java
@@ -1,18 +1,29 @@
 package se.acoder.todo.activities;
 
 import se.acoder.todo.R;
-import se.acoder.todo.R.layout;
-import se.acoder.todo.R.menu;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.widget.TextView;
 
 public class StatisticsActivity extends BaseActivity {
+	TextView text1, text2;
+	int totalTasks, doneTasks;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_statistics);
+		
+		text1 = (TextView) findViewById(R.id.statistics_1);
+		text2 = (TextView) findViewById(R.id.statistics_2);
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		totalTasks = sp.getInt("totalTasks", 0);
+		doneTasks = sp.getInt("doneTasks", 0);
+		text1.setText("Total number of tasks: " + totalTasks);
+		text2.setText("Completed Tasks: " + doneTasks);
 	}
 
 }
